1eeb54b82f59aa5f4272f155dcad78a56a778e35
finelee03 <finelee03@snu.ac.kr>
2025-09-16 12:25:56 +0900

    fix(web): route /auth/* to backend & pin socket.io origin to Render - docs/js/config.js: expose PROD_BACKEND + legacy aliases - auth-boot.js: unify toAPI() and honor backend origin - socket-client.js, mine.js, me.js: use backend origin for io() calls

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,24 +1,6 @@
 (() => {
-  // ① Render API 도메인으로 교체
-  const API = "https://aud-api-dtd1.onrender.com";
-
-  // 전역 상수로 노출
-  window.PROD_BACKEND = API;
-
-  // ② 모든 상대 fetch를 API 오리진으로 보정 (간단 우회)
-  const _fetch = window.fetch.bind(window);
-  window.fetch = function(input, init) {
-    try {
-      const u = new URL(input, location.href);
-      if (u.origin === location.origin && /^\/(?:auth|api)\//.test(u.pathname)) {
-        const API = window.PROD_BACKEND || window.API_BASE;
-        if (API) {
-          const apiURL = new URL(u.pathname + u.search + u.hash, API);
-          return _fetch(apiURL.toString(), init);
-        }
-      }
-    } catch {}
-    return _fetch(input, init);
-  };
-
+  const API = "";
+  window.PROD_BACKEND = API;  // 신규 코드가 참조
+  window.API_ORIGIN   = API;  // 레거시 코드가 참조
+  window.API_BASE     = API;  // 레거시 alias
 })();
